4083f43cf86c457dd805c659b6c1567e1051d6c8
thevoidwatches <43529522+thevoidwatches@users.noreply.github.com>
2022-11-22 19:17:22 -0500

    Cleaning dicemod.py

--- dicemod.py
+++ dicemod.py
@@ -1,11 +1,11 @@
 import discord
 
 async def critCheck(arg, result, improved):
-  #closes immediately if this is a roll for minions
+  # Closes immediately if this is a roll being made by minions
   if "min" in arg or "minion" in arg or "m" in arg:
     return ""
 
-  #sets critical value
+  # Sets the critical value
   critVal = 20
   if improved == True:
     if "imp4" in arg:
@@ -24,26 +24,27 @@ async def critCheck(arg, result, improved):
     return ""
 
 async def degrees(degrees, DC, crit):
-  #takes in the degrees of success, the DC, and if the roll was a crit, and returns a verbose string string of it
+  # Takes in the degrees of success, the DC, and if the roll was a crit, and returns a verbose string summarizing it.
   if crit != "":
     degrees += 1
   if degrees >= 0:
-    if degrees <= 1:
-      ret = "Against a DC of " + str(DC) + ", that's 1 degree of success!"
+    if degrees == 1:
+      return "Against a DC of " + str(DC) + ", that's 1 degree of success!"
     else:
-      ret = "Against a DC of " + str(DC) + ", that's " + str(degrees) + " degrees of success!"
+      return "Against a DC of " + str(DC) + ", that's " + str(degrees) + " degrees of success!"
   else:
     if degrees == -1:
-      ret = "Against a DC of " + str(DC) + ", that's 1 degree of failure!"
+      return "Against a DC of " + str(DC) + ", that's 1 degree of failure!"
     else:
-      ret = "Against a DC of " + str(DC) + ", that's " + str(degrees * -1) + " degrees of failure!"
-  return ret
+      return "Against a DC of " + str(DC) + ", that's " + str(degrees * -1) + " degrees of failure!"
 
-async def printResult(result, sa, hp, bonus, total, crit, degrees):
+async def printResult(result, hp, bonus, total, crit, degrees):
+  # Takes in the various aspects of the roll, and returns a verbose string summarizing the result.
   crit5 = ""
   if crit != "":
     crit5 = "That's an effective **" + str(total + 5) + "!** "
 
   if result == total:
     return "Rolled a **" + str(result) + crit + "!** " + crit5 + degrees
-  return "Rolled a " + str(result) + sa + hp + bonus + ", for a total of **" + str(total) + crit + "**! " + crit5 + degrees
+  else
+    return "Rolled a " + str(result) + hp + bonus + ", for a total of **" + str(total) + crit + "**! " + crit5 + degrees
